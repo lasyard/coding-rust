18fc62b58258f1e69a4d42a2b7a2ff06ec3922ff
Lasy <lasyard@yeah.net>
2025-09-28 17:50:01 +0800

    add fibonacci

---
+++ fibonacci/src/main.rs
@@ -0,0 +1,14 @@
+const N: u32 = 20;
+
+fn main() {
+    for i in {
+        let mut arr = [0; N as usize];
+        arr[1] = 1;
+        for i in 2..N {
+            arr[i as usize] = arr[(i - 1) as usize] + arr[(i - 2) as usize];
+        }
+        arr
+    } {
+        print!("{:?} ", i);
+    }
+}
