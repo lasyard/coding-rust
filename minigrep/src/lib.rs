9c86b352ca279f112c9a7eb9f0fd2587b515cda3
Lasyard <lasyard@yeah.net>
2026-08-13 11:29:42 +0800

    A minigrep

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,37 @@
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents.lines().filter(|line| line.contains(query)).collect()
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(search(query, contents), vec!["safe, fast, productive."]);
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(search_case_insensitive(query, contents), vec!["Rust:", "Trust me."]);
+    }
+}
